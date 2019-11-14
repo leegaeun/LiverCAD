629452af67f21983f41be00439b849949ec19994
leegaeun <ggelee93@gmail.com>
2019-11-14 13:45:51 +0900

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -1,10 +1,6 @@
-"""
-Mask R-CNN
-The main Mask R-CNN model implementation.
-Copyright (c) 2017 Matterport, Inc.
-Licensed under the MIT License (see LICENSE for details)
-Written by Waleed Abdulla
-"""
+ """ The following codes were modified from 
+ the original Mask R-CNN codes (https://github.com/matterport/Mask_RCNN.git). """ 
+
 
 import os
 import random
